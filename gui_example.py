6c42466230b5c176c65c34ac693ea1d83513a510
some-repository <patch.code2002@gmail.com>
2024-04-15 22:21:50 +0300

    Console output on button push

--- gui_example.py
+++ gui_example.py
@@ -1,8 +1,18 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget)
+from PyQt5.QtWidgets import (QApplication, QWidget, QGridLayout, QPushButton)
 
 class MainWindow (QWidget):
-    pass
+    def __init__ (self):
+        # Call original Qwidget constructor
+        super ().__init__ ()
+        self.setWindowTitle ("GUI example") # Set custom window title
+        grid = QGridLayout ()
+        self.setLayout (grid) # Create grid for widgets placing
+        button_1 = QPushButton ("Button name") # Create button and put it on grid
+        grid.addWidget (button_1, 0, 0)
+        button_1.clicked.connect (self.__button_1_handler) # Connect handler to "clicked" event
+    def __button_1_handler (self): # Create handler for "clicked" event
+        print ("Button pushed")
 
 def main ():
     app = QApplication (sys.argv)
